e12b82d90c318ed14962119afddc9e3815fcb0cb
roman <romanzvereff@gmail.com>
2022-04-13 16:29:13 +0300

    spring-@Scope-@Value-@PostConstruct-@PreDestroy-demo

--- java-spring-IoC/src/main/java/org/example/MusicPlayer.java
+++ java-spring-IoC/src/main/java/org/example/MusicPlayer.java
@@ -2,17 +2,29 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("singleton")
 public class MusicPlayer {
 
     private Music rockMusic;
     private Music classicalMusic;
     private Music rapMusic;
 
+    @Value("${musicPlayerModel}")
+    private String musicPlayerModel;
+
+    @Value("${musicPlayerYear}")
+    private int musicPlayerYear;
+
     public MusicPlayer() {
     }
+
     @Autowired
     public MusicPlayer(@Qualifier("rockMusic") Music rockMusic,
                        @Qualifier("classicalMusic") Music classicalMusic) {
@@ -39,7 +51,20 @@ public class MusicPlayer {
     }
 
     public void playMusic() {
+        System.out.println(this);
         System.out.println(rockMusic.getSongName() + " " + classicalMusic.getSongName() + " " + rapMusic.getSongName());
     }
 
+    @PreDestroy
+    private void destroy() {
+        System.out.println("MusicPlayer...stop and destroy!");
+    }
+
+    @Override
+    public String toString() {
+        return "MusicPlayer{" +
+                "musicPlayerModel='" + musicPlayerModel + '\'' +
+                ", musicPlayerYear=" + musicPlayerYear +
+                '}';
+    }
 }
--- java-spring-IoC/src/main/java/org/example/MusicService.java
+++ java-spring-IoC/src/main/java/org/example/MusicService.java
@@ -1,15 +1,31 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("prototype")
 public class MusicService {
+
+    private MusicPlayer musicPlayer;
 
     public MusicService() {
     }
 
-    private MusicPlayer musicPlayer;
+    @PostConstruct
+    private void init() {
+        System.out.println("MusicService...initialization!");
+    }
+
+    // Not working when @Scope("prototype")
+    @PreDestroy
+    private void destroy() {
+        System.out.println("MusicService...destroyed!");
+    }
 
     @Autowired
     public void setMusicPlayer(MusicPlayer musicPlayer) {
