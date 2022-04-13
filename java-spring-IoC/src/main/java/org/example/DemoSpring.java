6f856773d62c7b60e62558c40ae1a70d4784c2fc
roman <romanzvereff@gmail.com>
2022-04-13 14:00:06 +0300

    spring-anotations-demo

--- java-spring-IoC/src/main/java/org/example/DemoSpring.java
+++ java-spring-IoC/src/main/java/org/example/DemoSpring.java
@@ -7,16 +7,12 @@ public class DemoSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer =  context.getBean("musicPlayerBean", MusicPlayer.class);
-        MusicPlayer musicPlayer2 =  context.getBean("musicPlayerBean", MusicPlayer.class);
+        MusicPlayer musicPlayer =  context.getBean("musicPlayer", MusicPlayer.class);
 
-        boolean isEquals = musicPlayer == musicPlayer2;
-        System.out.println(isEquals);
+        Music classicalMusic = context.getBean("classicalMusic", Music.class);
 
-        musicPlayer2.setMusicPlayerYear(2022);
-
-        System.out.println(musicPlayer.getMusicPlayerYear());
-        System.out.println(musicPlayer2.getMusicPlayerYear());
+        musicPlayer.setMusic(classicalMusic);
+        musicPlayer.playMusic();
 
         context.close();
     }
